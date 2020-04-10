2a1601dcad06ff19b761fad9fc0cf53fcf3274d4
mnallamalli97 <mnallamalli97@gmail.com>
2020-04-10 15:42:24 -0700

    settings screen changed

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -16,7 +16,7 @@ public final class R {
         public static final int standard = 0x7f0800b2;
         public static final int text = 0x7f0800b9;
         public static final int text2 = 0x7f0800ba;
-        public static final int wrap_content = 0x7f0800d6;
+        public static final int wrap_content = 0x7f0800d5;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090007;
